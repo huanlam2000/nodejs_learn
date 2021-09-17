ed30e0815ab642eaa461e627c419a29b6d3a63d6
huanlam2000 <huantyn@gmail.com>
2021-09-17 21:17:26 +0700

    Controller & Route

--- src/index.js
+++ src/index.js
@@ -5,30 +5,29 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
 app.use(express.static(path.join(__dirname, "public")));
 
-// HTTP logger
-app.use(morgan("combined"));
-
-// template engine
-app.engine(
-  "hbs",
-  handlebars({
-    extname: ".hbs",
+// middleware
+app.use(
+  express.urlencoded({
+    extended: true,
   })
 );
+app.use(express.json());
+
+// HTTP logger
+// app.use(morgan("combined"));
+
+// Template engine
+app.engine("hbs",handlebars({
+    extname: ".hbs",
+}));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-// Trang chủ
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-//Trang News
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+//Routes init
+route(app);
 
 // Server lắng nghe cổng 3000
 app.listen(port, () => {
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const newsRouter = require("./news");
+const siteRouter = require("./site");
+
+function route(app) {
+  // Trang News
+  app.use("/news", newsRouter);
+
+  // Trang chủ
+  app.use("/", siteRouter);
+}
+
+module.exports = route;
